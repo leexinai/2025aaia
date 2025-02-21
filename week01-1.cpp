0137b693faf50e2cfd6905f69aa53f7955c1de3a
leexinai <shadowf230659340@gmail.com>
2025-02-21 12:01:37 +0800

    week01-6

---
+++ week01-1.cpp
@@ -0,0 +1,11 @@
+///week01-1.cpp要自己打好全部的檔名,後面要打.cpp
+///在瘋狂程設(練習模式)寫好程式碼,全選,Ctrl-C
+///CodeBlocks File-New-Empty 空白檔案 Crtl-s存檔
+#include <stdio.h>///使用standard
+
+int maun()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",a+b);
+}
